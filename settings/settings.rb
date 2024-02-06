0a7816d2adbeb4318e76457585aeddc0271c4373
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-02-05 21:34:32 -0500

    Update settings.rb
    use proper `File.join` to create file system path

--- settings/settings.rb
+++ settings/settings.rb
@@ -1,14 +1,14 @@
 module Settings
   @lock = Mutex.new
 
-  APP_DIR = Dir.home + "/.profanity"
+  APP_DIR = File.join(Dir.home, '.profanity')
   ##
   ## setup app dir
   ##
   FileUtils.mkdir_p APP_DIR
 
   def self.file(path)
-    APP_DIR + "/" + path
+    File.join(APP_DIR, path)
   end
 
   def self.read(file)
